261a4253692ce27f8d226c04d932c2ef5c64dfa2
AseelImad8 <aseelimad462@gmail.com>
2024-08-24 23:19:17 +0300

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+
+    return <nav>
+        <a href="#">Navbar</a>
+        <a href="#">home</a>
+        <a href="#">Employees</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </nav>
+}
